f6ec7312f654980134160de093c19f22f0e4f2ee
Hristiyan Trifonov <hristiyan@abelsoninfo.com>
2026-03-23 10:13:20 +0000

    Add tests for background job and fix test environment test suite

--- spec/jobs/trips/calculate_average_rating_job_spec.rb
+++ spec/jobs/trips/calculate_average_rating_job_spec.rb
@@ -1,5 +1,31 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Trips::CalculateMedianRatingJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Trips::CalculateAverageRatingJob, type: :job do
+  describe "#perform" do
+    before do
+      allow(Rails.logger).to receive(:info)
+    end
+
+    context "when there are no trips" do
+      it "logs that no trips were found" do
+        described_class.perform_now
+
+        expect(Rails.logger).to have_received(:info)
+                                  .with("[CalculateAverageRatingJob] No trips found.")
+      end
+    end
+
+    context "when trips exist" do
+      it "log the correct average rating" do
+        Trip.create!(name: "Trip with a 4 rating", rating: 4)
+        Trip.create!(name: "Trip with a 2 rating", rating: 2)
+        Trip.create!(name: "Trip with a 5 rating", rating: 5)
+
+        described_class.perform_now
+
+        expect(Rails.logger).to have_received(:info)
+                                  .with("[CalculateAverageRatingJob] Current average rating is 3.67 across 3 trips.")
+      end
+    end
+  end
 end
